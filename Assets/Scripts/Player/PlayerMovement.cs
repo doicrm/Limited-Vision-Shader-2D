9bb9151d03e29b576a8312512c49080494dacfd8
doicrm <40691503+doicrm@users.noreply.github.com>
2024-04-24 13:19:33 +0200

    Created simple player movement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+    float speedX, speedY;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        speedX = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        speedY = Input.GetAxisRaw("Vertical") * moveSpeed;
+        rb.velocity = new Vector2(speedX, speedY);
+    }
+}
