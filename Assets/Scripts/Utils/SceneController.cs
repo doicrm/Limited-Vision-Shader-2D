5b14f471b61386ec8b3ea1aab1c34a5589541aad
Doic <michalakradek2@gmail.com>
2024-04-24 17:05:32 +0200

    Optimized scene controller

--- Assets/Scripts/Utils/SceneController.cs
+++ Assets/Scripts/Utils/SceneController.cs
@@ -4,21 +4,36 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
+    public static SceneController Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            return;
+        }
+        Destroy(gameObject);
+    }
+
     void Update()
     {
         if (Input.GetKey(KeyCode.Escape))
             ExitGame();
+        if (Input.GetKey(KeyCode.N))
+            LoadNextScene();
     }
 
     public void LoadNextScene()
     {
-        if (SceneManager.GetActiveScene().buildIndex == SceneManager.sceneCount)
+        if (SceneManager.GetActiveScene().buildIndex >= SceneManager.sceneCount)
         {
             SceneManager.LoadScene(0);
             return;
         }
 
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
     public void ExitGame()
